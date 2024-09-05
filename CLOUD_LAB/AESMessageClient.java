f1a32a35b221c2924ce8e40a028e8bc523dde259
Ashish Goswami <116054951+Ashish6298@users.noreply.github.com>
2024-09-05 12:07:15 +0530

    Update AESMessageClient.java

--- CLOUD_LAB/AESMessageClient.java
+++ CLOUD_LAB/AESMessageClient.java
@@ -7,22 +7,36 @@ import javax.crypto.spec.SecretKeySpec;
 public class AESMessageClient {
 
     private static final String ALGORITHM = "AES";
-    private static final String STATIC_KEY_STRING = "QvZJafkl7MLEePlcRK7lCA=="; // Static key for both server and client
 
     public static void main(String[] args) {
         try {
-            // Message to send
-            String originalMessage = "Hello, server!";
-            System.out.println("Original message: " + originalMessage);
+            String keyString;
+            String encryptedMessage;
 
-            // Encrypt the message
-            String encryptedMessage = encryptMessage(originalMessage, STATIC_KEY_STRING);
-            System.out.println("Encrypted message: " + encryptedMessage);
+            // Connect to server and receive key
+            try (Socket socket = new Socket("localhost", 12345);
+                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                 PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
 
-            // Send encrypted message to server
-            String response = sendTcpRequest("localhost", 12345, encryptedMessage);
-            System.out.println("Server response: " + response);
+                // Receive AES key from server
+                keyString = in.readLine();
+                System.out.println("Received AES Key: " + keyString);
 
+                // Message to send
+                String originalMessage = "Hello, server!";
+                System.out.println("Original message: " + originalMessage);
+
+                // Encrypt the message
+                encryptedMessage = encryptMessage(originalMessage, keyString);
+                System.out.println("Encrypted message: " + encryptedMessage);
+
+                // Send encrypted message to server
+                out.println(encryptedMessage);
+
+                // Read response from server
+                String response = in.readLine();
+                System.out.println("Server response: " + response);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -36,23 +50,5 @@ public class AESMessageClient {
 
         byte[] encryptedMessageBytes = cipher.doFinal(message.getBytes());
         return Base64.getEncoder().encodeToString(encryptedMessageBytes);
-    }
-
-    private static String sendTcpRequest(String serverAddress, int port, String encryptedMessage) throws IOException {
-        StringBuilder response = new StringBuilder();
-        try (Socket socket = new Socket(serverAddress, port);
-             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
-
-            // Send encrypted message to server
-            out.println(encryptedMessage);
-
-            // Read response from server
-            String line;
-            while ((line = in.readLine()) != null) {
-                response.append(line).append("\n");
-            }
-        }
-        return response.toString();
     }
 }
